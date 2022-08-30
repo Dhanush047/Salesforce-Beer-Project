1fdc9abc645801ddc54d157850402b6c105f107f
Dhanush047 <dhanushadhityannagarajan@gmail.com>
2022-08-30 12:37:16 +0530

    data table added

---
+++ force-app/main/default/aura/DataTableApplication/DataTableApplication.app
@@ -0,0 +1,3 @@
+<aura:application extends="force:slds">
+    <c:DataTableComponent> </c:DataTableComponent>
+</aura:application>	
--- force-app/main/default/aura/DataTableComponent/DataTableComponentController.js
+++ force-app/main/default/aura/DataTableComponent/DataTableComponentController.js
@@ -1,5 +1,22 @@
 ({
-    myAction : function(component, event, helper) {
-
+    init: function (component, event, helper) 
+    {
+        component.set('v.column', [
+            {label: 'Beer Order Number', fieldName: 'Name', type: 'text'},
+            {label: 'Order Amount', fieldName: 'Order_Amount__c', type: 'Currency'},
+            {label: 'City ', fieldName: 'Billing_City__c', type: 'text'},
+            {label: 'State ', fieldName: 'Billing_State__c', type: 'text'}
+        ]);
+        var action = component.get('c.getdatatable');
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            //alert(state);
+            if(state === 'SUCCESS' || state==='DRAFT' ){
+                var responseValue = response.getReturnValue();
+                console.log('responseValue ', responseValue)
+                component.set('v.data', responseValue);
+            }
+        });
+        $A.enqueueAction(action);
     }
 })
