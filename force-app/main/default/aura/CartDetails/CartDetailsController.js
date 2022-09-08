4ea0d492466d686b1d3c1b81fb7f0b35f6976a53
Dhanush047 <dhanushadhityannagarajan@gmail.com>
2022-09-08 11:57:36 +0530

    Beer Explorer

--- force-app/main/default/aura/CartDetails/CartDetailsController.js
+++ force-app/main/default/aura/CartDetails/CartDetailsController.js
@@ -147,30 +147,33 @@ doApplyCoupon : function(component, event, helper)
     {
         component.set('v.isCheckout', true);
     },
-    doSaveAddress : function(component, event, helper)
+    doSaveAddress :  function(component, event, helper) 
     {
-        component.find('recordCreator').saveRecord(function(saveResult){
-            if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
-                // record is saved successfully
-                var resultsToast = $A.get("e.force:showToast");
-                resultsToast.setParams({
-                    "title": "Success",
-                    "type" : "success",
-                    "message": "The record was saved."
-                    
-                });
-                resultsToast.fire();
 
-            } else if (saveResult.state === "INCOMPLETE") {
-                // handle the incomplete state
-                console.log("User is offline, device doesn't support drafts.");
-            } else if (saveResult.state === "ERROR") {
-                // handle the error state
-                console.log('Problem saving contact, error: ' + JSON.stringify(saveResult.error));
-            } else {
-                console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
-            }
-        });
-    
-}    
+        var isValidAddress = helper.validate(component, event, helper);
+        alert(isValidAddress);
+        if(isValidAddress){
+            var userId = $A.get("$SObjectType.CurrentUser.Id");
+            component.set('v.addressBook.User__c',userId);
+            component.find('recordCreator').saveRecord(function(saveResult){
+                if(saveResult.state === 'SUCCESS' || saveResult.state === 'DRAFT'){
+                    var showToast = $A.get('e.force:showToast');
+                    showToast.setParams({
+                        "title" : "Record Saved",
+                        "type" : "Success",
+                        "message" : "Addressbook has been Save  "+saveResult.recordId
+                    });
+                    showToast.fire();
+                  
+                 //   component.set('v.isNewAddress', false);
+                } else if(saveResult.state === 'INCOMPLETE'){
+                    
+                }else if(saveResult.state === 'ERROR'){
+                    
+                }else{
+                    
+                }
+            });
+        }
+    }
 })
